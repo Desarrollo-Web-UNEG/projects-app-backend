3b86a5a4079135c6fe017dd77dd20c986a639ad4
Santacruzluis <luisantacruzkids@gmail.com>
2025-06-12 11:03:03 -0400

    [ADD] Creation of entities through TypeORM.

---
+++ src/modules/scores/scores.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Project } from '../project/project.entity';
+
+@Entity()
+export class Scores {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('float')
+  score: number;
+
+  @Column({ type: 'date', nullable: true })
+  date: Date;
+
+  @ManyToOne(() => Project, (project) => project.scores)
+  project: Project;
+} 
