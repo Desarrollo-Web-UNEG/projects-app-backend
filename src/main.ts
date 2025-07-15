ba33729279b51c675174df863955eb8c0b35db91
Santacruzluis <luisantacruzkids@gmail.com>
2025-07-14 20:22:31 -0400

    [FIX] se acomodo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
@@ -8,6 +9,18 @@ async function bootstrap() {
   // Agrega esta línea para ver logs de peticiones HTTP
   app.use(morgan('dev'));
 
-  await app.listen(3000);
+  // Configurar prefijo global para todas las rutas
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Projects App API')
+    .setDescription('API para la gestión de proyectos de la UNEG')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
